239e7a18095f37f6b74ffd9c9918f6e013e738e9
sates298 <stanislaw.wo@interia.pl>
2018-12-03 11:14:50 +0100

    implement Field, SixPointedStar and his Side

--- src/main/java/field/Field.java
+++ src/main/java/field/Field.java
@@ -4,6 +4,14 @@ public abstract class Field {
     int x;
     int y;
 
+    public Field(){
+        this.x = 0;
+        this.y = 0;
+    }
+    public Field(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
     public int getX() {
         return x;
     }
