3041a44fa3dc365fb07f27938f8936695565ccda
sates298 <stanislaw.wo@interia.pl>
2018-12-08 18:14:36 +0100

    add method toString in Board

--- src/main/java/server/board/Board.java
+++ src/main/java/server/board/Board.java
@@ -30,6 +30,20 @@ public abstract class Board {
     public abstract int sizeY();
     public abstract int sizeX();
 
+    public String fieldsToStiring(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("[ ");
+        for(int i=0; i<sizeY(); i++){
+            builder.append("[ ");
+            for (int j=0; j< sizeX(); j++){
+                builder.append(fields[j][i].toString());
+            }
+            builder.append("] , ");
+        }
+        builder.append("] ");
+        return builder.toString();
+    }
+
     public boolean isPawn(int x, int y) throws ArrayIndexOutOfBoundsException{
         if(x >= sizeX() || y >= sizeY()){
             throw new ArrayIndexOutOfBoundsException();
