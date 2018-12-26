4dc755237b51d0bbb4f2f17f09b6fd6da320c9e1
Paweł Wilkosz <pwilkosz5@gmail.com>
2018-12-26 14:18:58 +0100

    six pointed star sides testing

---
+++ src/test/java/serverTests/SixPointedStarSideTest.java
@@ -0,0 +1,124 @@
+package serverTests;
+
+import org.junit.Test;
+import server.board.Board;
+import server.board.SixPointedStar;
+import server.board.SixPointedStarSide;
+import server.creator.boardCreator.BoardCreator;
+import server.creator.boardCreator.SixPointedStarCreator;
+import server.field.Field;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class SixPointedStarSideTest {
+  @Test
+  public void testBottomOpposite() {
+    assertEquals(SixPointedStarSide.TOP,
+        SixPointedStarSide.BOTTOM.getOppositeSide());
+  }
+  @Test
+  public void testLeftBottomOpposite() {
+    assertEquals(SixPointedStarSide.RIGHT_TOP,
+        SixPointedStarSide.LEFT_BOTTOM.getOppositeSide());
+  }
+  @Test
+  public void testRightBottomOpposite() {
+    assertEquals(SixPointedStarSide.LEFT_TOP,
+        SixPointedStarSide.RIGHT_BOTTOM.getOppositeSide());
+  }
+
+  @Test
+  public void testTopOpposite() {
+    assertEquals(SixPointedStarSide.BOTTOM,
+        SixPointedStarSide.TOP.getOppositeSide());
+  }
+  @Test
+  public void testLeftTopOpposite() {
+    assertEquals(SixPointedStarSide.RIGHT_BOTTOM,
+        SixPointedStarSide.LEFT_TOP.getOppositeSide());
+  }
+  @Test
+  public void testRightTopOpposite() {
+    assertEquals(SixPointedStarSide.LEFT_BOTTOM,
+        SixPointedStarSide.RIGHT_TOP.getOppositeSide());
+  }
+
+  @Test
+  public void testBottomGetOppositeArea() {
+    BoardCreator bc = new SixPointedStarCreator();
+    Board board = bc.createBoard();
+    assertEquals(SixPointedStarSide.BOTTOM.getArea(board),
+        SixPointedStarSide.TOP.getOppositeArea((SixPointedStar)board));
+  }
+  @Test
+  public void testLeftBottomGetOppositeArea() {
+    BoardCreator bc = new SixPointedStarCreator();
+    Board board = bc.createBoard();
+    assertEquals(SixPointedStarSide.LEFT_BOTTOM.getArea(board),
+        SixPointedStarSide.RIGHT_TOP.getOppositeArea((SixPointedStar)board));
+  }
+  @Test
+  public void testRightBottomGetOppositeArea() {
+    BoardCreator bc = new SixPointedStarCreator();
+    Board board = bc.createBoard();
+    assertEquals(SixPointedStarSide.RIGHT_BOTTOM.getArea(board),
+        SixPointedStarSide.LEFT_TOP.getOppositeArea((SixPointedStar)board));
+  }
+
+  @Test
+  public void testTopGetOppositeArea() {
+    BoardCreator bc = new SixPointedStarCreator();
+    Board board = bc.createBoard();
+    assertEquals(SixPointedStarSide.TOP.getArea(board),
+        SixPointedStarSide.BOTTOM.getOppositeArea((SixPointedStar)board));
+  }
+  @Test
+  public void testLeftTopGetOppositeArea() {
+    BoardCreator bc = new SixPointedStarCreator();
+    Board board = bc.createBoard();
+    assertEquals(SixPointedStarSide.LEFT_TOP.getArea(board),
+        SixPointedStarSide.RIGHT_BOTTOM.getOppositeArea((SixPointedStar)board));
+  }
+  @Test
+  public void testRightTopGetOppositeArea() {
+    BoardCreator bc = new SixPointedStarCreator();
+    Board board = bc.createBoard();
+    assertEquals(SixPointedStarSide.RIGHT_TOP.getArea(board),
+        SixPointedStarSide.LEFT_BOTTOM.getOppositeArea((SixPointedStar)board));
+  }
+
+  @Test
+  public void testBottomGetFarthest() {
+    Field field = SixPointedStarSide.BOTTOM.getFarthest();
+    assertTrue(field.getX() == 12 && field.getY() == 16);
+  }
+  @Test
+  public void testLeftBottomGetFarthest() {
+    Field field = SixPointedStarSide.LEFT_BOTTOM.getFarthest();
+    assertTrue(field.getX() == 4 && field.getY() == 12);
+  }
+  @Test
+  public void testRightBottomGetFarthest() {
+    Field field = SixPointedStarSide.RIGHT_BOTTOM.getFarthest();
+    assertTrue(field.getX() == 16 && field.getY() == 12);
+  }
+
+  @Test
+  public void testTopGetFarthest() {
+    Field field = SixPointedStarSide.TOP.getFarthest();
+    assertTrue(field.getX() == 4 && field.getY() == 0);
+  }
+  @Test
+  public void testLeftTopGetFarthest() {
+    Field field = SixPointedStarSide.LEFT_TOP.getFarthest();
+    assertTrue(field.getX() == 0 && field.getY() == 4);
+  }
+  @Test
+  public void testRightTopGetFarthest() {
+    Field field = SixPointedStarSide.RIGHT_TOP.getFarthest();
+    assertTrue(field.getX() == 12 && field.getY() == 4);
+  }
+
+}
